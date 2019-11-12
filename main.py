7ae9a72f07620dd0756941098ee42555c9fc97bd
RogueDarkJedi <roguedarkjedi@users.noreply.github.com>
2019-11-11 20:16:28 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dccontroller import DogCamController
 from dcsocket import DogCamWebSocket
-import dcserver
 import asyncio
 
 async def main():
   CamControl = DogCamController()
   SocketServer = DogCamWebSocket()
+  
+  print(f"Camera Controller at {str(CamControl)} and Socket Server at {str(SocketServer)}")
   
   while True:
     try:
