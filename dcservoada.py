253b4b990844005dff458050dace9888bd859f89
RogueDarkJedi <roguedarkjedi@users.noreply.github.com>
2020-05-14 15:49:40 -0700

    Update dcservoada.py

--- dcservoada.py
+++ dcservoada.py
@@ -9,10 +9,10 @@ ServoLib = ServoKit(channels=16)
 
 class DogCamServoAda(DogCamServoBase):
   def __init__(self, InName, InPin, ZeroAngle=0.0, Steps=1.0, LowerBounds=0.0, UpperBounds=180.0, PulseWidthMin=1000, PulseWidthMax=2000):
-    
+
     ServoLib.servo[InPin].actuation_range = UpperBounds
     ServoLib.servo[InPin].set_pulse_width_range(PulseWidthMin, PulseWidthMax)
-    
+
     super().__init__(InName, InPin, InZeroAngle=ZeroAngle, InSteps=Steps, InLowerBounds=LowerBounds, InUpperBounds=UpperBounds)
 
   def _MoveToPosition(self, angle):
